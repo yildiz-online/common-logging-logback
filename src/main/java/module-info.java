07bfc6da3fb650adc0dc96d66b90dde60225d3c0
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-02-13 12:00:38 +0100

    [FIX] Remove splunk libraries.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,6 @@ module be.yildizgames.common.logging.logback {
     requires be.yildizgames.common.exception;
     requires be.yildizgames.common.util;
     requires be.yildizgames.common.logging;
-    requires splunk.library.javalogging;
     requires logback.classic;
     requires slf4j.api;
     requires logback.core;
